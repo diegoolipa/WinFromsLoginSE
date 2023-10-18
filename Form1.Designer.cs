6a51f61e13df7335c48c62a5035b0bb3dcdcf7ea
Diego Frank Lipa Choque <d.diegolipa@gmail.com>
2023-10-18 12:20:29 -0500

    Login

---
+++ Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace Login
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panel1 = new Panel();
+            pictureBox4 = new PictureBox();
+            btnCerrar = new PictureBox();
+            btnMinimizar = new PictureBox();
+            label1 = new Label();
+            txtUser = new TextBox();
+            txtPass = new TextBox();
+            btnLogin = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            pictureBox3 = new PictureBox();
+            lblMensajeError = new Label();
+            linkLabel1 = new LinkLabel();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)btnCerrar).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)btnMinimizar).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(0, 121, 107);
+            panel1.Controls.Add(pictureBox4);
+            panel1.Dock = DockStyle.Left;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(250, 291);
+            panel1.TabIndex = 0;
+            panel1.MouseDown += panel1_MouseDown;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(41, 62);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(148, 170);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 12;
+            pictureBox4.TabStop = false;
+            // 
+            // btnCerrar
+            // 
+            btnCerrar.Image = (Image)resources.GetObject("btnCerrar.Image");
+            btnCerrar.Location = new Point(725, 12);
+            btnCerrar.Name = "btnCerrar";
+            btnCerrar.Size = new Size(27, 25);
+            btnCerrar.SizeMode = PictureBoxSizeMode.Zoom;
+            btnCerrar.TabIndex = 1;
+            btnCerrar.TabStop = false;
+            btnCerrar.Click += btnCerrar_Click;
+            // 
+            // btnMinimizar
+            // 
+            btnMinimizar.Image = (Image)resources.GetObject("btnMinimizar.Image");
+            btnMinimizar.Location = new Point(692, 12);
+            btnMinimizar.Name = "btnMinimizar";
+            btnMinimizar.Size = new Size(27, 25);
+            btnMinimizar.SizeMode = PictureBoxSizeMode.Zoom;
+            btnMinimizar.TabIndex = 2;
+            btnMinimizar.TabStop = false;
+            btnMinimizar.Click += btnMinimizar_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Century Gothic", 20.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(413, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(194, 33);
+            label1.TabIndex = 3;
+            label1.Text = "LOGIN DIEGO";
+            // 
+            // txtUser
+            // 
+            txtUser.Font = new Font("Century Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            txtUser.Location = new Point(324, 80);
+            txtUser.Name = "txtUser";
+            txtUser.Size = new Size(380, 33);
+            txtUser.TabIndex = 1;
+            txtUser.Text = "Usuario";
+            // 
+            // txtPass
+            // 
+            txtPass.Font = new Font("Century Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            txtPass.Location = new Point(324, 137);
+            txtPass.Name = "txtPass";
+            txtPass.Size = new Size(380, 33);
+            txtPass.TabIndex = 2;
+            txtPass.Text = "Contraseña";
+            // 
+            // btnLogin
+            // 
+            btnLogin.Font = new Font("Century Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            btnLogin.Location = new Point(324, 219);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(380, 35);
+            btnLogin.TabIndex = 3;
+            btnLogin.Text = "ACCEDER";
+            btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(324, 62);
+            label2.Name = "label2";
+            label2.Size = new Size(47, 15);
+            label2.TabIndex = 7;
+            label2.Text = "Usuario";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(324, 119);
+            label3.Name = "label3";
+            label3.Size = new Size(67, 15);
+            label3.TabIndex = 8;
+            label3.Text = "Contraseña";
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(324, 183);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(27, 25);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 9;
+            pictureBox3.TabStop = false;
+            // 
+            // lblMensajeError
+            // 
+            lblMensajeError.AutoSize = true;
+            lblMensajeError.ForeColor = Color.White;
+            lblMensajeError.Location = new Point(356, 187);
+            lblMensajeError.Name = "lblMensajeError";
+            lblMensajeError.Size = new Size(95, 15);
+            lblMensajeError.TabIndex = 10;
+            lblMensajeError.Text = "Mensaje de error";
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Font = new Font("Century Gothic", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            linkLabel1.LinkColor = Color.White;
+            linkLabel1.Location = new Point(432, 262);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(175, 17);
+            linkLabel1.TabIndex = 0;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Ha olvidado la contrañena?";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(15, 15, 15);
+            ClientSize = new Size(764, 291);
+            Controls.Add(linkLabel1);
+            Controls.Add(lblMensajeError);
+            Controls.Add(pictureBox3);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(btnLogin);
+            Controls.Add(txtPass);
+            Controls.Add(txtUser);
+            Controls.Add(label1);
+            Controls.Add(btnMinimizar);
+            Controls.Add(btnCerrar);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            Opacity = 0.9D;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            Load += Form1_Load;
+            MouseDown += Form1_MouseDown;
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)btnCerrar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)btnMinimizar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private PictureBox btnCerrar;
+        private PictureBox btnMinimizar;
+        private Label label1;
+        private TextBox txtUser;
+        private TextBox txtPass;
+        private Button btnLogin;
+        private Label label2;
+        private Label label3;
+        private PictureBox pictureBox3;
+        private Label lblMensajeError;
+        private LinkLabel linkLabel1;
+        private PictureBox pictureBox4;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,46 @@
+using System.Runtime.InteropServices;
+
+namespace Login
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //para arrastrar INICIO Dlipa
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hwnd, int wmsg, int wparam, int lparam);
+        //para arrastrar FIN
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+    }
+}
